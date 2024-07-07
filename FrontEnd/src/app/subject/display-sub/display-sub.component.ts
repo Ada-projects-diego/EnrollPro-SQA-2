53e4c367d181aeb27ab267dbb21926b69bfb1601
djaumandreuonda <diego.jaumandreuonda@ada.ac.uk>
2024-07-07 17:24:50 +0100

    feat: subject component

---
+++ FrontEnd/src/app/subject/display-sub/display-sub.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+@Component({
+  selector: 'app-display-sub',
+  templateUrl: './display-sub.component.html',
+  styleUrls: ['./display-sub.component.css']
+})
+export class DisplaySubComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  subjectList:any=[];
+  ModalTitle!: string;
+  ActivateAddEditSubComp:boolean=false; 
+  sub:any;
+
+  ngOnInit(): void {
+    this.refreshSubList();
+  }
+
+  addClick(){
+    this.sub={
+      subjectId: 0,
+      subjectName:"",
+      subjectDescription:"",
+      courseId: null
+    }
+    this.ModalTitle="Add Subject";
+    this.ActivateAddEditSubComp=true;
+  }
+  
+  deleteClick(item: { subjectId: any; }){
+      this.service.deleteSubject(item.subjectId).subscribe(data =>{
+        this.refreshSubList();
+      })
+  }
+
+  closeClick(){
+    this.ActivateAddEditSubComp=false;
+    this.refreshSubList();
+  }
+
+  refreshSubList(){
+    this.service.getSubList().subscribe(data=>{
+      this.subjectList = data
+    });
+  }
+}
+
+
